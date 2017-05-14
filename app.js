09bcd0f9351dc2590674e6076f28fcdd9dc4c5bd
Benedict Suarez <benedictsuarez888@gmail.com>
2017-05-14 15:57:00 +0800

    Fixed <a href>

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.post('/status', function (req, res) {
 
 	connection.query('UPDATE requests SET status="Done" WHERE request_id = ?;', [request_id], function (err, row, fields) {
 		var html = 'You successfully updated the service status of ID Request: ' + request_id + ' to "Done."' +
-					'<br><a href=/index>Click here to go back in Current Services</a>'
+					'<br><a href=/>Click here to go back in Current Services</a>'
 		res.send(html)
 	})
 	connection.end();
@@ -87,7 +87,7 @@ app.post('/payment', function (req, res) {
 
 	connection.query('UPDATE requests SET isPaid="T" WHERE request_id = ?;', [request_id1], function (err, row, fields) {
 		var html = 'You successfully updated the payment status of ID Request: ' + request_id1 + ' to "Paid."' +
-					'<br><a href=/index>Click here to go back in Current Services</a>'
+					'<br><a href=/>Click here to go back in Current Services</a>'
 		res.send(html)
 	})
 	connection.end();
